e62e0dcd7588878f8187b6ced976e247c29a0cc6
927joanna <sot.joanna@gmail.com>
2023-04-20 15:42:35 +0800

    get CAT!! using api key.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import Message from "./components/Message";
 
-function App() {
+const App = () => {
+  const [errCat, setErrCat] = useState('');
+  const [goodCat, setGoodCat] = useState([]);
+
+  const getCat = async(tag='cute') => {
+    const res = await fetch('https://api.thecatapi.com/v1/images/search', {
+          method: 'GET',
+          headers: {
+            'Content-Type': "application/json",
+            'x-api-key': import.meta.env.THECATAPI_API_KEY
+          },
+        });
+      
+    const data = await res.json();
+    console.log(data)
+    
+    if (res.status === 200) {
+      setGoodCat(data)
+    } else {
+      const link = 'https://http.cat/' + res.status + '.jpg';
+      setErrCat(link)
+    }
+  }
+
   return (
     <div>
-      <h2>GA SEI</h2>
+      <button onClick={getCat}>CAT!!</button>
+      { errCat && <img src={errCat}></img>}
+      {goodCat.map((item, idx) => {
+        return (
+        <Message key={item.id} id={item.id} sender='cat' content={item.url}/>
+        )
+      })}
+
+
     </div>
   );
 }
