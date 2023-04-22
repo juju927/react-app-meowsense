f69dbafdf156baa292b86d56483426078b05f1a1
927joanna <sot.joanna@gmail.com>
2023-04-23 01:21:27 +0800

    proper scrollin

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Message from './Message.jsx'
 import styles from './Chat.module.css'
 
 const Chat = (props) => {
+  const bottomRef = useRef()
+
+  useEffect(()=> {
+    bottomRef.current.scrollIntoView()
+  }, [props.chatlog])
+
   return(
   <div className={ styles['chatarea'] } >
   {props.chatlog.map((item, idx) => {
@@ -10,6 +16,7 @@ const Chat = (props) => {
       <Message key={idx} sender={item.sender} type={item.type} content={item.content} />
     )
   })}
+    <div ref={bottomRef} />
   </div>
   )
 }
