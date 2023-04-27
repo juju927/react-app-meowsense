85a64c74572d7a127af647c5e4bbd343a31ccc9f
927joanna <sot.joanna@gmail.com>
2023-04-27 21:50:34 +0800

    the link in the navlink works now
    thx bing!!!!!

--- src/components/ChatListItem.jsx
+++ src/components/ChatListItem.jsx
@@ -4,14 +4,14 @@ import ProfilePic from './ProfilePic'
 import styles from './ChatListItem.module.css'
 
 const ChatListItem = (props) => {
-
+  
   return (
     <NavLink to={props.navLink} className={ styles['navlink'] }>
       <div className={ styles['chatlist-item-container'] }>
         <ProfilePic type='chatlistpfp' img={props.img} />
         <div className={ styles['left'] }>
           <h1 className={ styles['name'] }>{props.name}</h1>
-          <i>powered by <a href={props.apiLink} target="_blank">{props.api}</a></i>
+          <i>powered by <a href={props.apiLink} target="_blank" onClick={(e)=> e.stopPropagation()}>{props.api}</a></i>
         </div>
       </div>
     </NavLink>  
